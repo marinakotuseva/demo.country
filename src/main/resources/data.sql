73053beb7f9e9bdbda0946a3927336d14bfc3383
Unknown <avesutok@gmail.com>
2019-04-03 11:56:06 +0300

    format

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 
-INSERT INTO countries (id, name) VALUES (1, 'Россия');
-INSERT INTO countries (id, name) VALUES (2, 'Бразилия');
-INSERT INTO countries (id, name) VALUES (3, 'Франция');
+insert into countries (name) values ('Россия');
+insert into countries (name) values ('Бразилия');
+insert into countries (name) values ('Франция');
+
+
+insert into cities (name,country_id) values ('Москва', 1);
+insert into cities (name,country_id) values ('Бразилиа', 2);
+insert into cities (name,country_id) values ('Париж', 3);
